720435ee15938e774160103fdb59c12aa9dc590c
IcaroSukerman <s.icaro@gmail.com>
2023-11-16 00:43:08 -0300

    Adicionado exercicio-variaveis-5

---
+++ exercicio-variaveis-5.js
@@ -0,0 +1,8 @@
+let x1 = 1;
+let x2 = 1;
+let y1 = 1;
+let y2 = 4;
+
+let resultado = (x1 - x2) ** 2 + (y2 - y1) ** 2; //Formula aplicada
+console.log(resultado); // Resultado da formula
+console.log("O resultado entre a distancia dos pontos é " + Math.sqrt(resultado)); //Comando para culculo de raiz quadrada
