180c2f89c5138942cfbcaf49c14102b7a89c7eb5
IcaroSukerman <s.icaro@gmail.com>
2023-11-15 23:02:17 -0300

    Atualização de Atividade

---
+++ exercicio-variaveis-2.js
@@ -0,0 +1,8 @@
+let F = 73
+
+let C = (10 - 32) \* \frac{ 5 } { 9 }
+C = 10 \* \frac{ 5 } { 9 }
+C = (73 - 32) \* \frac{ 5 } { 9 }
+C ≅ 23
+
+//O seu programa deverá imprimir graus em Celsius com base no input recebido.
