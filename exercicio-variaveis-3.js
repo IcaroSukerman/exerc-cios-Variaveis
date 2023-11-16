99e152d4da04cf4cb2ed5018bf4af5a76011b779
IcaroSukerman <s.icaro@gmail.com>
2023-11-16 00:04:14 -0300

    Adicionado atividade 3

---
+++ exercicio-variaveis-3.js
@@ -0,0 +1,7 @@
+let tenis = 129.99; // Valor do tênis
+let seuDinheiro = 80; // Saldo na conta
+console.log(tenis - seuDinheiro);
+
+let sobra = tenis - seuDinheiro;
+let desconto = sobra * 100 / 129.99; //Formula o valor de desconto
+console.log(`Com a compra do tênis por desconto R$${seuDinheiro}, o valor de ${desconto}% de desconto`);
