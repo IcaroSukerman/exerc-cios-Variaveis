2ccf3ad3773e837e82e38354869605092976507b
IcaroSukerman <s.icaro@gmail.com>
2023-11-15 23:13:53 -0300

    Adicionado descrição dsa variaveis

--- exercicio-variaveis-2.js
+++ exercicio-variaveis-2.js
@@ -5,4 +5,4 @@ C = 10 \* \frac{ 5 } { 9 }
 C = (73 - 32) \* \frac{ 5 } { 9 }
 C ≅ 23
 
-//O seu programa deverá imprimir graus em Celsius com base no input recebido.
+//O seu programa deverá imprimir graus em Celsius com base no input recebido. Teste
